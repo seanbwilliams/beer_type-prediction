7106e35d112974e5cf3fe263b01991b75036b568
seanbwilliams <seanbwill@gmail.com>
2022-03-09 16:45:03 +1100

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='The Beer Type Prediction project uses a custom neural network to predict the type of beer based on a user's rating criteria',
+    author='Sean Williams',
+    license='',
+)
